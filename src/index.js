e77182bfded201038f6270b44247042c2254107d
PeJman <58425285+Pj-Thinker@users.noreply.github.com>
2021-09-18 14:40:04 +0430

    restructure the code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'splitting/dist/splitting.css';
+import 'splitting/dist/splitting-cells.css';
+import Splitting from 'splitting';
+
+Splitting({
+  target: '[data-splitting]',
+  by: 'chars',
+  key: null,
+});
