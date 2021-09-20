9bc67cae7fdea63a0187af1a6d1e36818f172988
PeJman <58425285+Pj-Thinker@users.noreply.github.com>
2021-09-20 20:56:40 +0430

    adding the auth

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import 'splitting/dist/splitting.css';
 import 'splitting/dist/splitting-cells.css';
-import Splitting from 'splitting';
+import Splitting, { add } from 'splitting';
+import netlifyIdentity from 'netlify-identity-widget';
 
 Splitting({
   target: '[data-splitting]',
   by: 'chars',
   key: null,
+});
+
+netlifyIdentity.init();
+
+document.querySelector('.loginBtn').addEventListener('click', () => {
+  netlifyIdentity.open();
+  netlifyIdentity.on('login', (user) => {
+    console.log('welcome user');
+  });
+});
+
+document.querySelector('.loginBtn2').addEventListener('click', () => {
+  const user = netlifyIdentity.currentUser();
+
+  fetch('/.netlify/functions/protected-function', {
+    headers: {
+      Authorization: 'Beare ' + user.token.access_token,
+    },
+  })
+    .then((x) => x.json())
+    .then(console.log('some'));
 });
